d4fbaa0d5a600095caaab513eee80f591ffc81e9
Nikelesleshwaran T <nikelesh2006@gmail.com>
2025-09-27 16:10:26 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var price =10
+var count=9
+var FruitName="Apple"
+var product="Asus"
+var tax=10
+console.log(price)
+console.log(product)
+var total=price*count
+console.log("the total price of the fruits is",total)
+
