98d0a429f9a4fba4693b1088a33b2d4c7606f149
Nikelesleshwaran T <nikelesh2006@gmail.com>
2025-09-29 16:49:15 +0530

    commit logics and return file only

---
+++ if_else.js
@@ -0,0 +1,25 @@
+rain =false
+if (rain){
+    console.log("take an umbrella")
+}
+else{
+    console.log("no need to take an umbrella")
+}
+
+/*question 2*/
+let homework="yes"
+if (homework==="yes"){
+    console.log("well done")
+}
+else{
+    console.log("complete your homework")
+}
+/*question 3*/
+
+let cookies="no"
+if (cookies==="yes"){
+    console.log("you can have a cookie")
+}
+else{
+    console.log("no cookies for you")
+}
---
+++ return.js
@@ -0,0 +1,12 @@
+function myname(){
+    return "hello nikelesh"
+}
+var a=myname()
+console.log(a)
+
+function myage(a,b){
+    return (a+b)
+
+}
+var a=myage(90,10)
+console.log(a)
