c0b4a46c2cfe270b4d46edaeb80473f3df89da98
Nikelesleshwaran T <nikelesh2006@gmail.com>
2025-09-29 13:16:59 +0530

    commit all the files

---
+++ demo.js
@@ -0,0 +1,14 @@
+function samsung(){
+   console.log("this is samsung phone")
+}
+function oppo(){
+    console.log("this is oppo phone")
+}
+
+function iphone()
+{
+    console.log("this is an iphone")
+}
+samsung()
+oppo()
+iphone()
---
+++ exercise.js
@@ -0,0 +1,16 @@
+var a=10
+var b=50
+function add(){
+    console.log(a+b)
+}
+add()
+var f_actor="vijay"
+var f_player="MS Dhoni"
+var f_movie="varanam aayiram"
+function fav(){
+    console.log("my favourite actor is "+f_actor)
+    console.log("my favourite player is "+f_player)
+    console.log("my favourite movie is "+f_movie)
+}
+fav()
+
