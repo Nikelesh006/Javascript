81cf21bc1a4686cd13b2d4daa5e597e8c50810e5
Nikelesleshwaran T <nikelesh2006@gmail.com>
2025-09-29 13:51:53 +0530

    push the remaining file

---
+++ addfun.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    console.log(a+b)
+
+}
+add(10,40)
+
+function area(length,breadth)
+{
+    var a=length*breadth
+    console.log("the area of rectangle is",a)
+    console.log(length,breadth)
+}
+area(10,50)
+
+
+
