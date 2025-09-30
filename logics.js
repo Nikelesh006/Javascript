dbdacd3d454deccf5e33ab350f86b4b1d0fa6ab8
Nikelesleshwaran T <nikelesh2006@gmail.com>
2025-09-30 15:53:25 +0530

    commit logics code changes

---
+++ logics.js
@@ -0,0 +1,15 @@
+console.log(false&&true)
+console.log(false||true)
+var season="summer"
+if(season=="spring"){
+    console.log("enjoy the blooming flowers")
+}
+if(season=="summer"){
+    console.log("enjoy the sunshine")
+}
+if (season=="autumn"){
+    console.log("Admire the colorful leaves")
+}
+if(season=="winter"){
+    console.log("Bunddle up and stay warm")
+}
