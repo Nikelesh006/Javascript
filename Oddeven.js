305340a4ce25cd4f99ed4cc14566397dca2669b0
Nikelesleshwaran T <nikelesh2006@gmail.com>
2025-10-02 10:45:27 +0530

    commit logics code changes

---
+++ Oddeven.js
@@ -0,0 +1,16 @@
+var a=9
+if(a%2==0){
+    console.log("It's an even number")
+}
+else{
+    console.log("It's an odd number")
+}
+/*question 2*/
+
+var b ="e"
+if(b=="a"||b=="i"||b=="e"||b=="o"||b=="u"){
+    console.log("It's an vowel ")
+}
+else {
+    console.log("It's an consonants")
+}
